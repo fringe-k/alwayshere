2b020068ef37dd3fe5ef227e4b943b5fa496b0f9
fringe_k <1151712307@qq.com>
2019-05-15 10:38:56 +0800

    r

--- pages/writing/writing.js
+++ pages/writing/writing.js
@@ -19,7 +19,54 @@ Page({
         checked2: false
     },
     days: ['1天后','2天后','3天后','4天后','5天后','6天后','7天后'],
-    daystosent:"未选择"
+    daystosent:"未选择",
+    chooseSize: false,
+    animationData: {}
+  },
+
+  chooseSezi: function (e) {
+    console.log(1);
+    // 用that取代this，防止不必要的情况发生
+    var that = this;
+    // 创建一个动画实例
+    var animation = wx.createAnimation({
+      // 动画持续时间
+      duration: 500,
+      // 定义动画效果，当前是匀速
+      timingFunction: 'linear'
+    })
+    // 将该变量赋值给当前动画
+    that.animation = animation
+    // 先在y轴偏移，然后用step()完成一个动画
+    animation.translateY(200).step()
+    // 用setData改变当前动画
+    that.setData({
+      // 通过export()方法导出数据
+      animationData: animation.export(),
+      // 改变view里面的Wx：if
+      chooseSize: true
+    })
+    // 设置setTimeout来改变y轴偏移量，实现有感觉的滑动
+    setTimeout(function () {
+      animation.translateY(0).step()
+      that.setData({
+        animationData: animation.export()
+      })
+    }, 200)
+  },
+  
+  sending: function (e) {
+    var that = this;
+    var animation = wx.createAnimation({
+      duration: 1000,
+      timingFunction: 'linear'
+    })
+    that.animation = animation
+    animation.translateY(200).step()
+    that.setData({
+      animationData: animation.export(),
+      chooseSize: false
+    })
   },
 
 daysPickerChange:function(e){
