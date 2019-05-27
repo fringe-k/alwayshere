c75bb44f6bb4ad6802a9e90fd0d3f42a3563308a
fringe_k <1151712307@qq.com>
2019-05-27 19:15:28 +0800

    Ta说完成

--- pages/main/main.js
+++ pages/main/main.js
@@ -7,7 +7,17 @@ Page({
    */
   data: {
     togetherdate: 360,
-    nextdate: 360
+    nextdate: 360,
+    addImg:"",
+    avatarUrl:""
+  },
+  onLoad: function (options) {
+    let that = this;
+    let gbd = getApp().globalData;
+    that.setData({
+      addImg: gbd.host + "/resource/image/add",
+      avatarUrl: gbd.userInfo.avatarUrl
+    })
   },
   toPageInfo: function () {
     wx.navigateTo({
@@ -27,7 +37,5 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    app=getApp();
-  },
+ 
 })
